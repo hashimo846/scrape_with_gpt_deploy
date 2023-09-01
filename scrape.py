19ecb4622275d77ea8f23250ce1f044ecd2add44
hashimo846 <ayaenum@gmail.com>
2023-09-01 15:03:14 +0900

    implement get_access_token and get_refresh_token functions in scrape.py

--- scrape.py
+++ scrape.py
@@ -11,6 +11,7 @@ import shutil
 import stat
 from pathlib import Path
 import selenium
+from typing import List, Dict
 
 # ロガーの初期化
 logger = log.init(__name__, DEBUG)
@@ -47,3 +48,40 @@ def scrape_all(url_list:List[str] = ['']) -> str:
         return None
     else:
         return ''.join(texts)
+
+# Octoparseの動作確認用
+OCTOPARSE_USERNAME = os.getenv('OCTOPARSE_USERNAME')
+OCTOPARSE_PASSWORD = os.getenv('OCTOPARSE_PASSWORD')
+OCTOPARSE_API_BASE_URL = 'https://openapi.octoparse.com/'
+
+# アクセストークンの取得
+def get_access_token(username:str = OCTOPARSE_USERNAME, passward:str = OCTOPARSE_PASSWORD, base_url:str = OCTOPARSE_API_BASE_URL) -> Dict:
+    path = 'token'
+    header = {"Content-Type": "application/json"}
+    body = {
+        'username':username,
+        'password':passward,
+        'grant_type':'password'
+    }
+    return requests.post(base_url+path, headers=header, json=body, timeout = 3.0).json()
+
+# アクセストークンの有効期限更新
+def refresh_access_token(refresh_token:str, base_url:str = OCTOPARSE_API_BASE_URL, ) -> Dict:
+    path = 'token'
+    header = {"Content-Type": "application/json"}
+    body = {
+        'refresh_token':refresh_token,
+        'grant_type':'refresh_token'
+    }
+    return requests.post(base_url+path, headers=header, json=body, timeout = 3.0).json()
+
+
+def main():
+    response = get_access_token()
+    logger.debug(log.format('アクセストークン取得', response))
+    sleep(1)
+    response = refresh_access_token(response['data']['refresh_token'])
+    logger.debug(log.format('アクセストークン更新', response))
+
+if __name__ == "__main__":
+    main()
